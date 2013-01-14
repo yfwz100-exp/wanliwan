3cf7ddd15d683bd4c009849cb329ff3576487fdc
qzr <iloveyouliping@sina.cn>
2013-01-14 20:28:01 +0800

    更改了一下目录

---
+++ controllers/user.js
@@ -0,0 +1,79 @@
+
+/*
+ * GET users listing.
+ */
+
+var User = require('../my_modules/user');
+
+exports.checkLogin = function checkLogin(req, res) {
+  return req.session.user;
+}
+
+exports.list = function(req, res){
+  if (req.session.user) {
+    res.render('list', {
+      layout: 'layout'
+    });
+  } else {
+    res.redirect('/login');
+  }
+};
+
+exports.registerView = function registerView(req, res) {
+  res.render('register');
+};
+exports.register = function register(req, res) {
+  if (req.body.user.name && req.body.user.pass && req.body.user.repass
+      && req.body.user.pass == req.body.user.repass) {
+    var user = new User(req.body.user);
+    user.save(function (err) {
+      if (! err) {
+        res.render('done', {
+          link: '/login',
+          message: 'Successfully register!'
+        });
+      } else {
+        res.render('error', {
+          link: '/register',
+          message: err
+        });
+      }
+    });
+  } else {
+    req.render('done', {
+      link: '/login',
+      message: 'The user has been registered.'
+    });
+  }
+};
+
+exports.loginView = function loginView(req, res) {
+  res.render('login');
+};
+exports.login = function login(req, res) {
+  var username = req.body.user.name;
+  var password = req.body.user.pass;
+  User.get(username, function(err, user) {
+    if (user && username == user.name && password == user.password) {
+      req.session.user = user;
+      res.render('done', {
+        message: 'Successfully login!',
+        link: '/users'
+      });
+    } else {
+      res.render('error', {
+        message: 'Wrong username or password...',
+        link: '/login'
+      });
+    }
+  });
+};
+
+exports.logout = function logout(req, res) {
+  req.session.user = null;
+  res.render('done', {
+    'link': '/login',
+    'message': 'Successfully logout!'
+  });
+}
+
---
+++ dbs/settings.js
@@ -0,0 +1,8 @@
+//数据库具体配置
+//db是数据库名
+
+module.exports = {
+  cookieSecret: 'wanliwanbyus',
+  db: 'qing',
+  host: 'localhost'
+};
