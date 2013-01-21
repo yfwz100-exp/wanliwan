42cd65b66f00913e0361006c10034ada0aebd497
yfwz100 <yfwz100@yeah.net>
2013-01-21 12:28:48 +0800

    [Structure] Add text model.

--- my_modules/text.js
+++ my_modules/text.js
@@ -1,7 +1,39 @@
-var mgs = require("mongoose");
+var mongoose = require('mongoose');
 
-var textSchema = new mgs.Schema({
-  content : String,    
-  time    : Date,
-  email   : String
+var textSchema = new mongoose.Schema({
+  // 转发层次；为空即原创
+  parent: [String],
+  // 文字内容
+  body: {
+    type: String,
+    required: true
+  },
+  // 作者的Email 
+  author: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    required: true
+  },
+  // 该文字的状态，如果需要审核，可为未审核状态。
+  state: String,
+  // 发表日期
+  date: {
+    type: Date,
+    'default': Date.now
+  },
+  // 标签列表
+  tag: [String],
+  // 访问该 text 的唯一标识符
+  uri: {
+    type: String,
+    unique: true
+  }
 });
+
+// 以 uri 的字典顺序建立索引
+textSchema.index({
+  uri: 1
+});
+
+module.exports = mongoose.model('posts', textSchema);
